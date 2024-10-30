d504fc33778a98fd463898486200196d1af22ff9
sebastian.gaviriag <sebastian.gaviriag@udea.edu.co>
2024-10-30 18:20:54 -0500

    Primer commit

---
+++ Areas.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    float r1, r2, r3;
+
+    cout<<"Ingrese el valor del radio de la primera circunferencia ";
+    cin>>r1;
+
+    cout<<"Ingrese el valor del radio de la segunda circunferencia ";
+    cin>>r2;
+
+    cout<<"Ingrese el valor del radio de la tercera circunferencia ";
+    cin>>r3;
+
+    cout<<"La suma de los radios es: "<<r1 + r2 + r3;
+    
+    
+    // for (int i=0; i<=10; i++){
+    //     std::cout<<i<<"\n";
+    // }
+
+    
+    system("pause");
+    return 0;
+}
